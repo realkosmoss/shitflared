c6029914f34183ceae4fdf4990431093836342cc
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2026-01-08 02:52:06 +0100

    Refactor metrics.go for cleaner code structure

--- ingress/origins/metrics.go
+++ ingress/origins/metrics.go
@@ -1,8 +1,6 @@
 package origins
 
-import (
-	"github.com/prometheus/client_golang/prometheus"
-)
+import "github.com/prometheus/client_golang/prometheus"
 
 const (
 	namespace = "cloudflared"
@@ -18,23 +16,26 @@ type metrics struct {
 	dnsResolverRequests *prometheus.CounterVec
 }
 
-func (m *metrics) IncrementDNSUDPRequests() {
-	m.dnsResolverRequests.WithLabelValues("udp").Inc()
-}
-
-func (m *metrics) IncrementDNSTCPRequests() {
-	m.dnsResolverRequests.WithLabelValues("tcp").Inc()
-}
+func (m *metrics) IncrementDNSUDPRequests() { m.dnsResolverRequests.WithLabelValues("udp").Inc() }
+func (m *metrics) IncrementDNSTCPRequests() { m.dnsResolverRequests.WithLabelValues("tcp").Inc() }
 
 func NewMetrics(registerer prometheus.Registerer) Metrics {
-	m := &metrics{
-		dnsResolverRequests: prometheus.NewCounterVec(prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: subsystem,
-			Name:      "dns_requests_total",
-			Help:      "Total count of DNS requests that have been proxied to the virtual DNS resolver origin",
-		}, []string{"protocol"}),
+	cv := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      "dns_requests_total",
+		Help:      "Total count of DNS requests that have been proxied to the virtual DNS resolver origin",
+	}, []string{"protocol"})
+
+	if err := registerer.Register(cv); err != nil {
+		if are, ok := err.(prometheus.AlreadyRegisteredError); ok {
+			if existing, ok := are.ExistingCollector.(*prometheus.CounterVec); ok {
+				cv = existing
+			}
+		} else {
+			panic(err)
+		}
 	}
-	registerer.MustRegister(m.dnsResolverRequests)
-	return m
+
+	return &metrics{dnsResolverRequests: cv}
 }
